ce95c7272f83b5985e14f2e5168d19861218c11b
Moe Ghashim <mohanadgh@gmail.com>
2026-02-05 15:45:33 -0600

    Remove legacy project files

--- mission-control/convex/activities.ts
+++
@@ -1,8 +0,0 @@
-import { query } from "./_generated/server";
-
-export const listLatest = query({
-  args: {},
-  handler: async (ctx) => {
-    return await ctx.db.query("activities").order("desc").take(10);
-  },
-});
